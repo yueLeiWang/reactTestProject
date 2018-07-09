4a328072c0bb6446567d067411a04d78e2eb2081
wangyuelei <lshmilyily@sohu.com>
2018-07-09 12:34:11 +0800

    Signed-off-by: wangyuelei <lshmilyily@sohu.com>

---
+++ src/common/routes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { HashRouter } from 'react-router-dom'
+import { Route, Redirect } from 'react-router'
+import Container from 'container'
+import Login from 'pages/login'
+import createHistory from 'history/createBrowserHistory'
+
+const history = createHistory()
+const location = history.location
+
+const routes = (
+  <HashRouter>
+    <div>
+      <Route path="/" component={Container} />
+      <Route path="/login" component={Login} />
+      {location.hash === '#/' ? <Redirect to='/login' /> : ''}
+    </div>
+  </HashRouter>
+  // 以上使用 HashRouter 是为了 github 上展示方便，以上路由写法的缺陷在于加载 login 页时候会把其他页面也加载出来，
+  // 项目中可使用如下写法
+  // <Switch>
+  //   <Route exec path="/" component={Container} />
+  //   <Route exec path="/login" component={Login} />
+  // </Switch>
+)
+
+export default routes
